36893d5446ec7632fc2f8c1e726dd710d0120df2
leyder154611 <leyder154611@users.noreply.github.com>
2020-10-30 07:20:59 -0500

    deploy to Domain

--- resources/js/components/Configuration.js
+++ resources/js/components/Configuration.js
@@ -1,4 +1,4 @@
-const url_principal = "http://74.208.128.74/plesk-site-preview/escaparatelolita.com/https/74.208.128.74/";
+const url_principal = "https://elescaparatedelolita.com/";
 //const url_principal = "http://127.0.0.1:8000/";
 const url_images = url_principal+'images/';
 
